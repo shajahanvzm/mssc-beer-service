e9098d5b61f6ed31171f6d4a028239ec974c6001
shajahan <shajahanvzm@gmail.com>
2020-06-06 16:44:19 +0530

    Beer Repository created

---
+++ src/main/java/com/sha/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.sha.msscbeerservice.repositories;
+
+import com.sha.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
