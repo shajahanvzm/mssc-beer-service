e0d6173e64c59844429342c8a8cdcc7d65af8d94
shajahan <shajahanvzm@gmail.com>
2020-06-08 16:34:03 +0530

    json property

--- src/main/java/com/sha/msscbeerservice/web/modal/BeerDto.java
+++ src/main/java/com/sha/msscbeerservice/web/modal/BeerDto.java
@@ -1,5 +1,6 @@
 package com.sha.msscbeerservice.web.modal;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,6 +29,8 @@ public class BeerDto {
     private BeerStyleEnum beerStyle;
     @Null
     private Integer version;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @NotNull
     @Positive
     private BigDecimal price;
@@ -36,8 +39,10 @@ public class BeerDto {
     private Long upc;
     private Integer quantityOnHand;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
 
